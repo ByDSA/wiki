760d064186202d71ec9c9d0f67e9bf824a65e02f
Daniel Sales <dsa21may@gmail.com>
2021-08-27 19:33:37 +0200

    first commit

---
+++ backup.js
@@ -0,0 +1,19 @@
+const { AppBackup, getLastPartOf, logSection } = require("datils");
+
+const PROJECT_FOLDER = __dirname;
+const DEST = process.argv[2] || PROJECT_FOLDER;
+require("dotenv").config({path: `${PROJECT_FOLDER}/.env`});
+
+const appBackup = new AppBackup({
+  path: PROJECT_FOLDER,
+  dest: DEST
+});
+appBackup.addDB({
+  type: 'postgres',
+  host: process.env.DB_HOST,
+  name: process.env.DB_NAME,
+  password: process.env.DB_PASS,
+  username: process.env.DB_USER
+});
+appBackup.addFile(".env");
+appBackup.make();
